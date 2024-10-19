190ad308016f3e197793b6ca55822f25e995ec38
sammy508 <chapagainsamir9@gmail.com>
2024-10-19 14:23:23 +0545

    dynamic table in selenium

---
+++ tables/dynamic_table.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+driver.maximize_window()
+
+time.sleep(3)
+username = driver.find_element(By.XPATH, "//input[@placeholder='Username']")
+username.send_keys("Admin")
+
+passw = driver.find_element(By.NAME, "password").send_keys("admin123")
+
+driver.find_element(By.XPATH, "//button[@type='submit']").click()
+time.sleep(2)
+driver.find_element(By.XPATH, "//*[@id='app']/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a").click()
+time.sleep(5)
+driver.find_element(By.XPATH, "//span[normalize-space()='User Management']").click()
+
+time.sleep(3)
+driver.find_element(By.XPATH, "//*[@id='app']/div[1]/div[1]/header/div[2]/nav/ul/li[1]/ul/li").click()
+time.sleep(5)
+
+rows = len(driver.find_elements(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div"))
+# print(f"total num of users : {(rows)}")
+
+time.sleep(5)
+
+count = 0
+for r in range(rows):
+    status = driver.find_element(By.XPATH, "//*[@id='app']/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div['+str(r)+']/div/div[5]").text
+
+    if status== "Enabled":
+        count+=1
+
+time.sleep(10)
+print(f"Total enabled user are {count}")
+print(f"Total disabled user are {rows -count}")
+driver.quit()
+
+# 9702586472
--- tables/static_table.py
+++ tables/static_table.py
@@ -25,6 +25,23 @@ print(data)
 # Read all the rows and columns from table\
 
 for r in range(2,row+1):
+
     for c in range(1, col + 1):
-     data=   driver.find_element(By.XPATH, " //*[@id='HTML1']/div[1]/table//tr[r]/td[c]").text    # it called dynamic xpath or a 
-     print(data)
+   
+     data=   driver.find_element(By.XPATH, " //*[@id='HTML1']/div[1]/table//tr["+str(r)+"]/td["+str(c)+"]").text    # it called dynamic xpath or a 
+     print(data, end="   ")  # end gives the space between two item and print it on tabular form
+    print() 
+
+
+# Read data based on condition
+
+for r in range(2,row+1):
+
+    auth_name =   driver.find_element(By.XPATH, " //*[@id='HTML1']/div[1]/table//tr["+str(r)+"]/td[2]").text   
+    if auth_name == "Mukesh":
+       book_name =   driver.find_element(By.XPATH, " //*[@id='HTML1']/div[1]/table//tr["+str(r)+"]/td[1]").text
+       price =   driver.find_element(By.XPATH, " //*[@id='HTML1']/div[1]/table//tr["+str(r)+"]/td[4]").text
+       print(book_name,":", " ", price)
+ # output
+#  Learn Java : Mukesh price
+# Master In Selenium : Mukesh  price
