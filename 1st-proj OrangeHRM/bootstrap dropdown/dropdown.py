4db190539973bb493dd208b194642af4924c9246
sammy508 <chapagainsamir9@gmail.com>
2024-10-27 10:51:21 +0545

    Bootstrap dropdown in selenium

---
+++ 1st-proj OrangeHRM/bootstrap dropdown/dropdown.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+import time
+driver = webdriver.Chrome()
+
+driver.get("https://www.dummyticket.com/dummy-ticket-for-visa-application/")
+driver.maximize_window()
+
+time.sleep(5)
+driver.find_element(By.XPATH, "//*[@id='select2-billing_country-container']").click()
+driver.implicitly_wait(5)
+
+countrieslist = driver.find_elements(By.XPATH,"//*[@id='billing_country']/*")
+
+time.sleep(5)
+for country in countrieslist:
+    if country.text == "Argentina":
+        country.click()
+        break
+
+time.sleep(5)
+
+driver.quit()
